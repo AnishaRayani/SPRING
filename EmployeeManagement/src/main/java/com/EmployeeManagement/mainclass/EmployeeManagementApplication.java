f23983976e7e2afd34999e7ccfd03199eb5085a7
AnishaRayani <anisha.rayani76@gmail.com>
2019-09-10 16:12:25 +0530

    Revised

--- EmployeeManagement/src/main/java/com/EmployeeManagement/mainclass/EmployeeManagementApplication.java
+++ EmployeeManagement/src/main/java/com/EmployeeManagement/mainclass/EmployeeManagementApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.EmployeeManagement.mainclass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
